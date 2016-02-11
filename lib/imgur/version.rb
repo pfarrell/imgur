2eb2172ef68b573f8429c2f97954227eb950c9e1
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-02-10 22:29:02 -0800

    git it started

---
+++ lib/imgur/version.rb
@@ -0,0 +1,3 @@
+module Imgur
+  VERSION = "0.1.0"
+end
---
+++ spec/imgur_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Imgur do
+  it 'has a version number' do
+    expect(Imgur::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
